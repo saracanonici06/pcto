eb806b0dd97308db307dad7044fbd36332db435e
saracanonici06 <124669076+saracanonici06@users.noreply.github.com>
2023-02-07 10:28:25 +0100

    Add files via upload

---
+++ abcd.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+char x = 'a';
+char y = 'b';
+
+printf(" x era: %c\n", carattere);
+printf(" y era: %c\n", carattere);
+printf("x è diventato: %d\n", carattere);
+printf("y  è diventato: %d\n", carattere);
+}
